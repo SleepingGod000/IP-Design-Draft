f6454f411be81b063ec56969ca2d22aee60ad3f2
SleepingGod000 <sgz000@yahoo.com>
2017-10-26 21:43:47 +0800

    added Product Tracking Page

--- js/purchaseHistory-datatable.js
+++ js/purchaseHistory-datatable.js
@@ -1,6 +1,13 @@
 // Call the dataTables jQuery plugin
+
 $(document).ready(function() {
   $('#purchaseHistory-datatable').DataTable({
 	  responsive: true
   });
+  
+  var receiptTable = $('#purchaseHistory-datatable').DataTable();
+  $('#purchaseHistory-datatable tbody').on( 'click', 'tr', function () {
+	  $('#receiptDescription').find('#receiptDescriptionLabel').text(receiptTable.row(this).data()[0])
+	  $('#receiptDescription').find('#receiptContent').text(receiptTable.row(this).data()[0])
+} );
 });
